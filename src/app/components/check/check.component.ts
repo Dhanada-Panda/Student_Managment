90f5f91790957b02ea9e6b71d0e3f71fd306144b
Dhanada Panda <dhanada.panda@godigit.com>
2025-09-19 13:12:37 +0530

    updated.

--- src/app/components/check/check.component.ts
+++ src/app/components/check/check.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-check',
   templateUrl: './check.component.html',
-  styleUrls: ['./check.component.css']
+  styleUrls: ['./check.component.css'],
 })
 export class CheckComponent {
   payment = {
@@ -15,6 +15,39 @@ export class CheckComponent {
     expiryDate: '12/Aug/2025',
     paymentMode: 'UPI',
     paidOn: '02/Sep/2025 12:20 pm',
-    lastUpdated: '02/Sep/2025 12:20 pm'
+    lastUpdated: '02/Sep/2025 12:20 pm',
+  };
+  asba = {
+    premiumStatus: 'Excess',
+    paymentStatus: 'Refund',
+    asbaRegNumber: 'ICICI28334349934234',
+    requestedAmount: 10000,
+    premiumAmount: 10000,
+    expiryDate: '12/Aug/2025',
+    paymentMode: 'UPI',
+    paidOn: '02/Sep/2025 12:20 pm',
+    lastUpdated: '02/Sep/2025 12:20 pm',
+  };
+  cheque = {
+    premiumStatus: 'Sufficient',
+    paymentStatus: 'Bounced',
+    asbaRegNumber: 'ICICI28334349934234',
+    premiumAmount: 10000,
+    paidOn: '02/Sep/2025 12:20 pm',
+    lastUpdated: '02/Sep/2025 12:20 pm',
+  };
+  notpaid = {
+    premiumAmount: 10000,
+    lastUpdated: '02/Sep/2025 12:20 pm',
+  };
+  online = {
+    premiumStatus: 'Deficit',
+    paymentStatus: 'Pending',
+    asbaRegNumber: 'ICICI28334349934234',
+    requestedAmount: 10000,
+    premiumAmount: 10000,
+    paymentMode: 'NEFT',
+    paidOn: '02/Sep/2025 12:20 pm',
+    lastUpdated: '02/Sep/2025 12:20 pm',
   };
 }
